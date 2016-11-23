120b496e815f5318837ba7c912ab50c00f2ab3a8
Felix Fietkau <nbd@nbd.name>
2016-11-23 21:00:38 +0100

    json: add uci based example mapping
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- run-example.sh
+++ run-example.sh
@@ -1,1 +1,7 @@
-exec ./scald -x json_dir=$PWD/examples/json -xjson_models=tr-181 -x device_file=$PWD/examples/data/cwmp-device.json -p $PWD/plugins/scapi_json.so
+exec ./scald \
+	-x json_dir=$PWD/examples/json \
+	-x json_models=tr-181 \
+	-x device_file=$PWD/examples/data/cwmp-device.json \
+	-x uci_confdir=$PWD/examples/config \
+	-x cwmp_local_addr=127.0.0.1:80 \
+	-p $PWD/plugins/scapi_json.so
